c9bba76fc925a30d91876c344b4f0fb213ac8a58
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2013-05-08 17:10:34 +0100

    Working with tests, up-to Polygon creation.

--- __init__.py
+++ __init__.py
@@ -8,12 +8,14 @@ Spherical gridcell area-overlap calculations.
 
 import itertools
 import math
+import numpy as np
 
 
 def convert_latlon_to_xyz(lat, lon):
     z = math.sin(lat)
-    x = z * math.cos(lon)
-    y = z * math.sin(lon)
+    cos_lat = math.cos(lat)
+    x = cos_lat * math.cos(lon)
+    y = cos_lat * math.sin(lon)
     return (x, y, z)
 
 
@@ -21,9 +23,7 @@ def convert_xyz_to_latlon(x, y, z):
     mod_sq = (x * x + y * y + z * z)
     if abs(mod_sq) < 1e-7:
         raise ValueError('Point too close to zero for lat-lon conversion.')
-    mod = math.sqrt(mod_sq)
-    x, y, z = [p / mod for p in (x, y, z)]
-    lat = math.asin(z)
+    lat = math.asin(z / math.sqrt(mod_sq))
     lon = math.atan2(y, x)
     return (lat, lon)
 
@@ -39,11 +39,13 @@ class SphPoint(object):
         self.lon = lon
         self._p3d = None
 
-    @property
     def as_xyz(self):
         if self._p3d is None:
             self._p3d = convert_latlon_to_xyz(self.lat, self.lon)
         return self._p3d
+
+    def as_latlon(self):
+        return (self.lat, self.lon)
 
     def copy(self):
         return SphPoint(self)
@@ -51,6 +53,34 @@ class SphPoint(object):
     def antipode(self):
         x, y, z = self.as_xyz()
         return SphPoint(*convert_xyz_to_latlon(-x, -y, -z))
+
+    def __eq__(self, other):
+        return np.allclose(self.as_xyz(), other.as_xyz(), rtol=1e-6, atol=1e-6)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def dot_product(self, other):
+        other = sph_point(other)
+        return sum([a * b for a, b in zip(self.as_xyz(), other.as_xyz())])
+
+    def cross_product(self, other):
+        ax, ay, az = self.as_xyz()
+        bx, by, bz = sph_point(other).as_xyz()
+        x, y, z = ((ay * bz - az * by),
+                   (az * bx - ax * bz),
+                   (ax * by - ay * bx))
+        return sph_point(convert_xyz_to_latlon(x, y, z))
+
+    def __str__(self):
+        def r2d(radians):
+            return radians * 180.0 / math.pi
+        return 'SphPoint({})'.format(self._ll_str())
+
+    def _ll_str(self):
+        def r2d(radians):
+            return radians * 180.0 / math.pi
+        return '({:+06.1f}d, {:+06.1f}d)'.format(r2d(self.lat), r2d(self.lon))
 
 
 def sph_point(point, in_degrees=False):
@@ -65,60 +95,57 @@ def sph_point(point, in_degrees=False):
     return SphPoint(*point)
 
 
-def sphpoints_dot_product(point_a, point_b):
-    xyz_a = sph_point(point_a).as_xyz()
-    xyz_b = sph_point(point_b).as_xyz()
-    return sum([a * b for a, b in zip(xyz_a, xyz_b)])
-
-
-def sphpoints_cross_product(point_a, point_b):
-    xa, ya, za = sph_point(point_a).as_xyz()
-    xb, yb, zb = sph_point(point_b).as_xyz()
-    x, y, z = ((ay * bz - az * by),
-               (az * bx - ax * bz),
-               (ax * by - ay * bx))
-    return sph_point(convert_xyz_to_latlon(x, y, z))
-
-
-class SphGcLineseg(object):
+class SphGcSeg(object):
     """ A great circle line segment, from 'A' to 'B' on the unit sphere. """
     def __init__(self, point_a, point_b):
         self.point_a = sph_point(point_a)
         self.point_b = sph_point(point_b)
-        self.pole = sphpoints_cross_product(self.point_b, self.point_a)
-        self.antipole = vector_negative(self.pole);
+        self.pole = self.point_b.cross_product(self.point_a)
 
     def has_point_on_left(self, point):
-        return sphpoints_dot_product(self.pole.as_xyz, point.as_xyz) > 0.0
+        return self.pole.dot_product(point) <= 0.0
 
-    def cos_angle_to_other(self, other):
+    def _cos_angle_to_other(self, other):
         # Angle from AB to AP
-        return math.acos(sphpoints_dot_product(self.pole, other.pole))
+        return self.pole.dot_product(other.pole)
+
+    def _cos_angle_to_point(self, point):
+        # Angle from AB to AP, where P = given point
+        if point == self.point_a:
+            return 1.0
+        seg2 = SphGcSeg(self.point_a, point)
+        result = self._cos_angle_to_other(seg2)
+        return result
 
     def angle_to_other(self, other):
         # Angle from AB to AP
-        return math.acos(self.cos_angle_to_other(other))
-
-    def cos_angle_to_point(self, point):
-        # Angle from AB to AP
-        seg2 = SphGcLineseg(self.point_a, point)
-        return math.acos(sphpoints_dot_product(self.pole, seg2.pole))
+        return math.acos(self._cos_angle_to_other(other))
 
     def angle_to_point(self, point):
         # Angle from AB to AP
-        return math.acos(self.cos_angle_to_point(point))
+        result = math.acos(self._cos_angle_to_point(point))
+        if abs(result) > 1e-7 and not self.has_point_on_left(point):
+            result = -result
+        return result
 
-    def intersect_points_with_other(self, other):
-        point_a = sphpoints_cross_product(self.pole, other.pole)
+    def intersection_points_with_other(self, other):
+        point_a = self.pole.cross_product(other.pole)
         point_b = point_a.antipode()
         return (point_a, point_b)
+
+    def __str__(self):
+        return 'SphGcSeg({!s} -> {!s}, pole={!s}>'.format(
+            self.point_a._ll_str(),
+            self.point_b._ll_str(),
+            self.pole._ll_str())
 
 
 class SphAcwConvexPolygon(object):
     def __init__(self, points=[], in_degrees=False, ordering_from_edge=None):
         self.points = [sph_point(point, in_degrees=in_degrees)
                        for point in points]
-        if len(self.points) < 3:
+        self.n_points = len(points)
+        if self.n_points < 3:
             raise ValueError('Polygon must have at least 3 points.')
         self._edges = None
         self._make_anticlockwise_convex(ordering_from_edge=ordering_from_edge)
@@ -137,36 +164,43 @@ class SphAcwConvexPolygon(object):
         return result
 
     def _make_anticlockwise_convex(self, ordering_from_edge=None):
-        if self._is_anticlockwise_convex(error_if_not=False):
+        if self._is_anticlockwise_convex():
             return
-        # Grab first edge if not given one
-        if ordering_from_edge is not None:
-            edge0 = ordering_from_edge
-        else:
+        if ordering_from_edge is None:
+            # Grab data from first edge, and 'freeze' the first two points
             edge0 = self.edge_gcs()[0]
+            points = self.points[2:]
+            fixed_points = self.points[:2]
+            fixed_angles = [-0.02, -0.01]
+        else:
+            # Use the given edge as a reference, so can sort *all* points
+            edge0 = ordering_from_edge
+            points = self.points
+            fixed_points = []
+            fixed_angles = []
         # Invalidate edges cache as we are about to reorder all the points
         self._edges = None
         # Calculate angles from reference edge to all other points
-        points = self.points
-        other_angles = [edge0.angle_to_point(p) for p in points[2:]]
+        angles = [edge0.angle_to_point(p) for p in points]
         eps = 1.0e-7
         max_valid_angle = math.pi + eps
         min_valid_angle = -eps
         # Sort into angle order (forward or reverse as seems best)
         # Fix the first two angles, to avoid any problems.
-        if all([x > min_valid_angle for x in other_angles]):
+        if all([x > min_valid_angle for x in angles]):
             # Try this with first 2 points in correct order
-            angles = [-0.02, -0.01] + other_angles
+            points = fixed_points + points
+            angles = fixed_angles + angles
         else:
-            # Try with 2 points in reverse order
-            points = points[2::-1] + points[2:]
-            angles = [-0.02, -0.01] + [-x for x in other_angles]
+            # Try with first 2 points in reverse order
+            points = fixed_points[::-1] + points
+            angles = fixed_angles + [-x for x in angles]
         # Check for all valid angles : NOTE first two points must be suitable.
         if any([a > max_valid_angle or a < min_valid_angle
                 for a in angles[2:]]):
             raise ValueError('SphAcwConvexPolygon cannot be made from given '
-                             'points, starting with first two.')
-        points_and_angles = zip(self.points, angles)
+                             'points.')
+        points_and_angles = zip(points, angles)
         points_and_angles_sorted = sorted(points_and_angles,
                                           key=lambda p_and_a: p_and_a[1])
         self.points = [p_and_a[0] for p_and_a in points_and_angles_sorted]
@@ -175,17 +209,16 @@ class SphAcwConvexPolygon(object):
                 'SphAcwConvexPolygon points cannot be reordered to make it '
                 'convex.')
 
-    @property
     def edge_gcs(self):
         if self._edges is None:
-            self._edges = [SphGcLineseg(points[i:i+2])
-                         for i in range(len(self.points))]
-            self._edges += [SphGcLineseg(points[-1], points[0])]
+            self._edges = [SphGcSeg(self.points[i], self.points[i+1])
+                           for i in range(self.n_points - 1)]
+            self._edges += [SphGcSeg(self.points[-1], self.points[0])]
         return self._edges
 
     def contains_point(self, point, in_degrees=False):
         point = sph_point(point, in_degrees=in_degrees)
-        return all(gc.has_point_on_left(point) for gc in self.edge_gcs)
+        return all(gc.has_point_on_left(point) for gc in self.edge_gcs())
 
     def area(self):
         angle_total = 0.0
@@ -201,7 +234,7 @@ class SphAcwConvexPolygon(object):
         result_points += [p for p in other.points if self.contains_point(p)]
         # Calculate all intersections of (extended) edges between A and B
         inters_ab += [gc_a.intersect_points_with_gc(gc_b)
-                      for gc_a in self.edge_gcs for gc_b in other.edge_gcs]
+                      for gc_a in self.edge_gcs() for gc_b in other.edge_gcs()]
         inters_ab = itertools.chain.from_iterable(inters_ab)  # ==flatten
         # Add to output: all A/B intersections within both areas
         result_points += [p for p in inters_ab
--- test_sph.py
+++ test_sph.py
@@ -4,13 +4,321 @@ Created on May 8, 2013
 @author: itpp
 '''
 
+import math
+
 from iris import tests
 
 import spherical_geometry as sph
 
-class TestSphGeom(tests.IrisTest):
+
+def spt(*args, **kwargs):
+    # wrap sph.sph_point, but default to in_degrees=True
+    in_degrees = kwargs.pop('in_degrees', True)
+    kwargs['in_degrees'] = in_degrees
+    return sph.sph_point(*args, **kwargs)
+
+def d2r(degrees):
+    return degrees / 180.0 * math.pi
+
+class TestConverts(tests.IrisTest):
     def test_convert_xyz_to_latlon(self):
-        self.assertEqual(sph.convert_xyz_to_latlon(0, 0, 1), (math.pi/2, 0.0))
+        def _test_xyz_latlon(test_xyz, expect_latlon):
+            result_latlon = sph.convert_xyz_to_latlon(*test_xyz)
+            self.assertArrayAllClose(result_latlon, expect_latlon)
+
+        _test_xyz_latlon((0, 0, 1), (d2r(90), 0.0))
+        _test_xyz_latlon((0, 0, 0.01), (d2r(90), 0.0))
+        _test_xyz_latlon((1, 0, 1.0), (d2r(45) , 0.0))
+        _test_xyz_latlon((-1, 0, 1.0), (d2r(45), d2r(180)))
+        _test_xyz_latlon((1.0, 0, 0), (0.0, 0.0))
+        _test_xyz_latlon((1.0, 1.0, 0), (0.0, d2r(45)))
+        _test_xyz_latlon((1.0, -1.0, 0), (0.0, -d2r(45)))
+        _test_xyz_latlon((-1.0, -1.0, 0), (0.0, -d2r(180 - 45)))
+        _test_xyz_latlon((0.0, -1.0, 0), (0.0, -d2r(90)))
+        _test_xyz_latlon((1, 1, -math.sqrt(2)), (-d2r(45), d2r(45)))
+        _test_xyz_latlon((math.sqrt(1.5), math.sqrt(0.5), math.sqrt(2.0)),
+                         (d2r(45), d2r(30)))
+
+    def test_convert_latlon_to_xyz(self):
+        def _test_latlon_xyz(test_latlon, expect_xyz):
+            result_xyz = sph.convert_latlon_to_xyz(*test_latlon)
+            self.assertArrayAllClose(result_xyz, expect_xyz, atol=1e-7)
+
+        rv2 = 1.0 / math.sqrt(2)
+        _test_latlon_xyz((d2r(90), 0.0), (0, 0, 1.0))
+        _test_latlon_xyz((d2r(45) , 0.0), (rv2, 0, rv2))
+        _test_latlon_xyz((d2r(45), d2r(180)), (-rv2, 0, rv2))
+        _test_latlon_xyz((0.0, 0.0), (1.0, 0, 0))
+        _test_latlon_xyz((0.0, d2r(45)), (rv2, rv2, 0))
+        _test_latlon_xyz((0.0, -d2r(45)), (rv2, -rv2, 0))
+        _test_latlon_xyz((0.0, -d2r(180 - 45)), (-rv2, -rv2, 0))
+        _test_latlon_xyz((0.0, -d2r(90)), (0.0, -1.0, 0))
+        _test_latlon_xyz((-d2r(45), d2r(45)), (0.5, 0.5, -rv2))
+
+class TestSphPoint(tests.IrisTest):
+    def test_point_basic(self):
+        pv4 = d2r(45)
+        pv6 = d2r(180) / 6
+        pt = sph.SphPoint(-pv4, pv6)
+        self.assertEqual(pt.lat, -pv4)
+        self.assertEqual(pt.lon, pv6)
+
+        latlon = pt.as_latlon()
+        self.assertArrayAllClose(latlon, (-pv4, pv6))
+
+        xyz = pt.as_xyz()
+        self.assertArrayAllClose(xyz, sph.convert_latlon_to_xyz(-pv4, pv6))
+
+        pt2 = pt.copy()
+        self.assertEqual(pt2, pt)
+        self.assertIsNot(pt2, pt)
+
+    def test_point_maker(self):
+        pt = spt((45, 45))
+        pv4 = d2r(45)
+        self.assertArrayAllClose((pt.lat, pt.lon), (pv4, pv4))
+
+    def test_point_antipode(self):
+        pt2 = spt((45, 45))
+        x1, y1, z1 = pt2.as_xyz()
+        pt2a = pt2.antipode()
+        x2, y2, z2 = pt2a.as_xyz()
+        self.assertArrayAllClose((x2, y2, z2), (-x1, -y1, -z1))
+
+    def test_points_dot_product(self):
+        pt1 = spt((0, 60))
+        pt2 = spt((0, 30))
+        self.assertAlmostEqual(pt1.dot_product(pt2), math.sqrt(3.0/4))
+
+        pt1 = spt((0, 60))
+        pt2 = spt((90, 30))
+        self.assertAlmostEqual(pt1.dot_product(pt2), 0.0)
+
+        pt1 = spt((45, 60))
+        pt2 = spt((-45, 60))
+        self.assertAlmostEqual(pt1.dot_product(pt2), 0.0)
+
+        pt1 = spt((45, 60))
+        pt2 = spt((45, 60))
+        self.assertAlmostEqual(pt1.dot_product(pt2), 1.0)
+
+        pt1 = spt((45, 60))
+        pt2 = spt((-45, 240))
+        self.assertAlmostEqual(pt1.dot_product(pt2), -1.0)
+
+    def test_points_cross_product(self):
+        pt1 = spt((0, 30))
+        pt2 = spt((0, 60))
+        pt = pt1.cross_product(pt2)
+        self.assertArrayAllClose(pt.as_latlon(), (d2r(90), 0))
+        pt = pt2.cross_product(pt1)
+        self.assertArrayAllClose(pt.as_latlon(), (-d2r(90), 0))
+
+        pt1 = spt((0, 30))
+        pt2 = spt((90, 60))
+        pt = pt1.cross_product(pt2)
+        px =  spt((0, -60))
+        self.assertArrayAllClose(pt.as_latlon(), px.as_latlon(), atol=1e-7)
+        pt = pt2.cross_product(pt1)
+        px =  spt((0, 120))
+        self.assertArrayAllClose(pt.as_latlon(), px.as_latlon(), atol=1e-7)
+
+        pt1 = spt((45, 60))
+        pt2 = spt((-45, 60))
+        pt = pt1.cross_product(pt2)
+        px =  spt((0, 150))
+        self.assertArrayAllClose(pt.as_latlon(), px.as_latlon(), atol=1e-7)
+        pt = pt2.cross_product(pt1)
+        px =  spt((0, -30))
+        self.assertArrayAllClose(pt.as_latlon(), px.as_latlon(), atol=1e-7)
+
+        pt1 = spt((45, 60))
+        pt2 = spt((45, 60))
+        with self.assertRaises(ValueError):
+            pt = pt1.cross_product(pt2)
+        with self.assertRaises(ValueError):
+            pt = pt2.cross_product(pt1)
+
+
+class TestSphGcSeg(tests.IrisTest):
+    def test_seg_basic(self):
+        p1 = spt((0, 30))
+        p2 = spt((0, 50))
+        seg = sph.SphGcSeg(p1, p2)
+        px = p2.cross_product(p1)
+        self.assertArrayAllClose(seg.pole.as_xyz(), px.as_xyz())
+
+    def test_seg_has_point_on_left(self):
+        seg = sph.SphGcSeg(spt((0, 30)), spt((0, 50)))
+        p3 = spt((-10, 70))
+        self.assertEqual(seg.has_point_on_left(p3), False)
+        p3 = spt((10, 70))
+        self.assertEqual(seg.has_point_on_left(p3), True)
+        p3 = spt((0.01, 70))
+        self.assertEqual(seg.has_point_on_left(p3), True)
+        
+        seg = sph.SphGcSeg(spt((45, 30)), spt((0, 50)))
+        p3 = spt((0, 0))
+        self.assertEqual(seg.has_point_on_left(p3), False)
+        p3 = spt((0, 40))
+        self.assertEqual(seg.has_point_on_left(p3), False)
+        p3 = spt((50, 50))
+        self.assertEqual(seg.has_point_on_left(p3), True)
+        p3 = spt((90, 0))
+        self.assertEqual(seg.has_point_on_left(p3), True)
+        
+    def test_seg_angle_to_other(self):
+        seg1 = sph.SphGcSeg(spt((0, 30)), spt((0, 50)))
+        seg2 = sph.SphGcSeg(spt((0, 30)), spt((90, 713)))
+        a = seg1.angle_to_other(seg2)
+        self.assertAlmostEqual(a, d2r(90))
+
+        seg1 = sph.SphGcSeg(spt((20, 30)), spt((40, 50)))
+        seg2 = sph.SphGcSeg(spt((20, 30)), spt((40, 50)))
+        a = seg1.angle_to_other(seg2)
+        self.assertAlmostEqual(a, 0.0)
+
+        seg1 = sph.SphGcSeg(spt((20, 30)), spt((40, 50)))
+        seg2 = sph.SphGcSeg(spt((40, 50)), spt((20, 30)))
+        a = seg1.angle_to_other(seg2)
+        self.assertAlmostEqual(a, d2r(180))
+
+    def test_seg_angle_to_point(self):
+        seg = sph.SphGcSeg(spt((0, 30)), spt((0, 50)))
+
+        a = seg.angle_to_point(spt((0, 70)))
+        self.assertAlmostEqual(a, 0.0)
+
+        a = seg.angle_to_point(spt((90, 20)))
+        self.assertAlmostEqual(a, d2r(90))
+
+        a = seg.angle_to_point(spt((-90, 20)))
+        self.assertAlmostEqual(a, d2r(-90))
+
+        # check colinear 'behind' start point
+        a = seg.angle_to_point(spt((0, -20)))
+        self.assertAlmostEqual(a, d2r(-180))  # NOTE: angle is negative?
+
+        # check same as far end
+        a = seg.angle_to_point(spt((0, 50)))
+        self.assertAlmostEqual(a, 0.0)
+
+        # check same as near end
+        a = seg.angle_to_point(spt((0, 30)))
+        self.assertAlmostEqual(a, 0.0)
+
+
+        a = seg.angle_to_point(spt((1, -20)))
+        self.assertAlmostEqual(a, d2r(178.695), delta=d2r(0.005))
+        a = seg.angle_to_point(spt((-1, -20)))
+        self.assertAlmostEqual(a, d2r(-178.695), delta=d2r(0.005))
+
+        a = seg.angle_to_point(spt((-20, 30)))
+        self.assertAlmostEqual(a, d2r(-90))
+        a = seg.angle_to_point(spt((1, 31)))
+        self.assertAlmostEqual(a, d2r(45), delta=d2r(0.1))
+        a = seg.angle_to_point(spt((-1, 31)))
+        self.assertAlmostEqual(a, d2r(-45), delta=d2r(0.1))
+        a = seg.angle_to_point(spt((-20, 50)))
+        self.assertAlmostEqual(a, d2r(-46.7808), delta=d2r(0.005))
+            # NOTE: this one not exact, because of cos scaling
+
+    def test_seg_intersection(self):
+        seg1 = sph.SphGcSeg(spt((0, 30)), spt((0, 50)))
+        seg2 = sph.SphGcSeg(spt((0, 30)), spt((60, 30)))
+        pts = seg1.intersection_points_with_other(seg2)
+        p1 = spt((0, 30))
+        self.assertArrayAllClose(pts[0].as_latlon(), p1.as_latlon(),
+                                 atol=1e-7)
+        self.assertArrayAllClose(pts[1].as_latlon(), p1.antipode().as_latlon(),
+                                 atol=1e-7)
+
+        seg1 = sph.SphGcSeg(spt((0, 45)), spt((0, 50)))
+        seg2 = sph.SphGcSeg(spt((50, 30)), spt((60, 30)))
+        pts = seg1.intersection_points_with_other(seg2)
+        p1 = spt((0, 30))
+        self.assertArrayAllClose(pts[0].as_latlon(), p1.as_latlon(),
+                                 atol=1e-7)
+        self.assertArrayAllClose(pts[1].as_latlon(), p1.antipode().as_latlon(),
+                                 atol=1e-7)
+
+        seg1 = sph.SphGcSeg(spt((-30, 0)), spt((30, 100)))
+        seg2 = sph.SphGcSeg(spt((20, -30)), spt((-20, 130)))
+        pts = seg1.intersection_points_with_other(seg2)
+        p1 = spt((0, 50))
+        self.assertArrayAllClose(pts[1].as_latlon(), p1.as_latlon(),
+                                 atol=1e-7)
+        self.assertArrayAllClose(pts[0].as_latlon(), p1.antipode().as_latlon(),
+                                 atol=1e-7)
+
+
+class TestSphPolygon(tests.IrisTest):
+    def test_polygon_create(self):
+        points = [(0, 0), (0, 50), (50, 50)]
+        poly = sph.SphAcwConvexPolygon(points, in_degrees=True)
+        pts = [spt(p) for p in points]
+#        print
+#        for i in range(len(pts)):
+#            print 'poly.point[{i}] = {poly_pt}, pts[{i}] = {pts_pt}'.format(
+#                i=i, poly_pt=poly.points[i], pts_pt=pts[i])
+        for i in range(len(pts)):
+            self.assertEqual(poly.points[i], pts[i])
+
+        # make it reversed : this forces it to correct the order
+        poly = sph.SphAcwConvexPolygon(points[::-1], in_degrees=True)
+#        print 'poly...'
+#        for i in range(len(pts)):
+#            print 'poly.point[{i}] = {poly_pt}, pts[{i}] = {pts_pt}'.format(
+#                i=i, poly_pt=poly.points[i], pts_pt=pts[i])
+        self.assertEqual(poly.points[0], pts[1])
+        self.assertEqual(poly.points[1], pts[2])
+        self.assertEqual(poly.points[2], pts[0])
+
+        # check fails on two points
+        points = [(0, 0), (0, 50)]
+        with self.assertRaises(ValueError):
+            poly = sph.SphAcwConvexPolygon(points, in_degrees=True)
+
+        # make a square-ish one
+        points = [(0, 0), (0, 50), (40, 50), (60, -10)]
+        poly = sph.SphAcwConvexPolygon(points, in_degrees=True)
+        pts = [spt(p) for p in points]
+        for i in range(len(pts)):
+            self.assertEqual(poly.points[i], pts[i])
+
+        # check it is ok to have the odd point out of order
+        points = [(0, 0), (0, 50), (40, 50), (60, -10), (15, 55)]
+        poly = sph.SphAcwConvexPolygon(points, in_degrees=True)
+        pts = [spt(p) for p in points]
+#        print 'poly...'
+#        for i in range(len(pts)):
+#            print 'poly.point[{i}] = {poly_pt}, pts[{i}] = {pts_pt}'.format(
+#                i=i, poly_pt=poly.points[i], pts_pt=pts[i])
+        self.assertEqual(poly.points[0], pts[0])
+        self.assertEqual(poly.points[1], pts[1])
+        self.assertEqual(poly.points[2], pts[4])
+        self.assertEqual(poly.points[3], pts[2])
+        self.assertEqual(poly.points[4], pts[3])
+
+        # check *not* ok if the first two points would no longer be adjacent
+        points = [(0, 0), (0, 50), (40, 50), (70, -10), (-20, 30)]
+        with self.assertRaises(ValueError):
+            poly = sph.SphAcwConvexPolygon(points, in_degrees=True)
+
+        # check *can* make this with a suitable reference edge to work from...
+        ref_seg = sph.SphGcSeg(spt((0, 0)), spt((-20, 30)))
+        poly = sph.SphAcwConvexPolygon(points, in_degrees=True,
+                                       ordering_from_edge=ref_seg)
+        pts = [spt(p) for p in points]
+#        print 'poly...'
+#        for i in range(len(pts)):
+#            print 'poly.point[{i}] = {poly_pt}, pts[{i}] = {pts_pt}'.format(
+#                i=i, poly_pt=poly.points[i], pts_pt=pts[i])
+        self.assertEqual(poly.points[0], pts[0])
+        self.assertEqual(poly.points[1], pts[4])
+        self.assertEqual(poly.points[2], pts[1])
+        self.assertEqual(poly.points[3], pts[2])
+        self.assertEqual(poly.points[4], pts[3])
 
 if __name__ == '__main__':
     tests.main()
